000db3ccb485acc4b8fb5c037f2462ca76ec52c7
mirahmad <mizahmadm@gmail>
2024-01-26 11:43:43 +0100

    well done4

---
+++ src/Demo_if.java
@@ -0,0 +1,16 @@
+public class Demo_if {
+
+    public static void main(String[] args) {
+
+        int age = 17;
+        age = age + 1;
+
+        if (age > 17) {
+            System.out.println("Dann darfst du da Computerspiel kaufen");
+
+
+        }
+
+
+    }
+}
