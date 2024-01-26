7a8022aec2c34d0c13500f580160370544b67a34
mirahmad <mizahmadm@gmail>
2024-01-26 12:27:24 +0100

    well done5

--- src/Demo_if.java
+++ src/Demo_if.java
@@ -3,11 +3,14 @@ public class Demo_if {
     public static void main(String[] args) {
 
         int age = 17;
-        age = age + 1;
+
 
         if (age > 17) {
             System.out.println("Dann darfst du da Computerspiel kaufen");
 
+
+        } else {
+            System.out.println("Du darfst das Spiel leier nicht kaufen,da du zu Jung bist");
 
         }
 
