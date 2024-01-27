0185bbb562807784318c5b86cc85b2fee931eeac
mirahmad <mizahmadm@gmail>
2024-01-27 10:00:10 +0100

    well done6

---
+++ src/Switch_Block.java
@@ -0,0 +1,22 @@
+public class Switch_Block {
+    public static void main(String[] args) {
+        int month = 2;
+        switch (month) {
+
+            case 1:
+                System.out.println("Januar");
+                break;
+            case 2:
+                System.out.println("Februar");
+                break;
+            case 3:
+                System.out.println("Marz");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+
+
+        }
+    }
+}
