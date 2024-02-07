5447983adb3a2bf821ed72af3627e50897af694c
mirahmad <mizahmadm@gmail>
2024-02-07 13:19:07 +0100

    arrays2

---
+++ src/Array2.java
@@ -0,0 +1,17 @@
+public class Array2 {
+    public static void main(String[] args) {
+
+        //  String[] names = new String[3]; erste variante
+       /* names[0]= "Hendrik";
+        names[1]="Paul";
+        names[2]="Andrey";
+        System.out.println(names[1]);
+    }*/
+        String[] names = {"Hendrik", "Paul", "Andrey"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
+    }
+}
