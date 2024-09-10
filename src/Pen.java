0c1b375844dc59e0ba1989c576c557e01fb2f394
Tel-ran.de <mizahmadm@gmail.com>
2024-09-10 11:58:10 +0200

    Test

---
+++ src/Pen.java
@@ -0,0 +1,13 @@
+public class Pen {
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private String color;
+
+}
+
